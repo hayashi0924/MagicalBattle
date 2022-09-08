6abe0f37bf26ffcb6c8c87aaec0f419540e44ab3
hayashi <hayashi.takuma0924@gmail.com>
2022-09-08 17:07:37 +0900

    [クラス削除]Spellクラスを削除　[フィールドの追加]SpellListクラス>攻撃力、タイプを追加

--- Main/src/character/magic/Magic.java
+++ Main/src/character/magic/Magic.java
@@ -8,26 +8,13 @@ import java.util.Map;
 
 public class Magic {
     private final SpellList spellList;
-    private final Spell spell;
 
-
-    private Magic(SpellList spellList, Spell spell){
+    private Magic(SpellList spellList){
         this.spellList = spellList;
-        this.spell = spell;
     }
 
     public static Magic init(final SpellList spellList){
-        final Map<SpellList, Spell> magic = new HashMap<>();
-        magic.put(SpellList.WEAK_FIRE, new Spell(AttackPointOfSpell.WEAK_FIRE, Type.init(Types.FIRE)));
-        magic.put(SpellList.WEAK_DIRT, new Spell(AttackPointOfSpell.WEAK_DIRT, Type.init(Types.DIRT)));
-        magic.put(SpellList.WEAK_WATER, new Spell(AttackPointOfSpell.WEAK_WATER, Type.init(Types.WATTER)));
-        magic.put(SpellList.FIRE, new Spell(AttackPointOfSpell.FIRE, Type.init(Types.FIRE)));
-        magic.put(SpellList.DIRT, new Spell(AttackPointOfSpell.DIRT, Type.init(Types.DIRT)));
-        magic.put(SpellList.WATER, new Spell(AttackPointOfSpell.WATER, Type.init(Types.WATTER)));
 
-        return new Magic(spellList, magic.get(spellList));
     }
-    public Spell spell(){
-        return this.spell;
-    }
+
 }
--- Main/src/character/magic/SpellList.java
+++ Main/src/character/magic/SpellList.java
@@ -1,16 +1,25 @@
 package character.magic;
 
-public enum SpellList {
-    WEAK_FIRE("弱い炎"),
-    WEAK_DIRT("弱い土"),
-    WEAK_WATER("弱い水"),
-    FIRE("炎"),
-    DIRT("土"),
-    WATER("水"),
+import character.attackPoint.AttackPoint;
+import type.Type;
+import type.Types;
+
+enum SpellList {
+    WEAK_FIRE("弱い炎", Type.init(Types.FIRE), AttackPointOfSpell.WEAK_FIRE),
+    WEAK_DIRT("弱い土", Type.init(Types.DIRT), AttackPointOfSpell.WEAK_DIRT),
+    WEAK_WATER("弱い水", Type.init(Types.WATTER), AttackPointOfSpell.WEAK_WATER),
+    FIRE("炎", Type.init(Types.FIRE), AttackPointOfSpell.FIRE),
+    DIRT("土", Type.init(Types.DIRT), AttackPointOfSpell.DIRT),
+    WATER("水", Type.init(Types.WATTER), AttackPointOfSpell.WATER),
     ;
     private final String name;
-    SpellList(final String name){
+    private final Type type;
+    private final AttackPoint attackPoint;
+
+    SpellList(final String name, final Type type, final AttackPoint attackPoint){
         this.name = name;
+        this.type = type;
+        this.attackPoint = attackPoint;
     };
 
     public String toString(){
