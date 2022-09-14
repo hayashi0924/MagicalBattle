0df48c00949ac60b3485626dca6d133555b9cbb3
hayashi <hayashi.takuma0924@gmail.com>
2022-09-14 09:10:50 +0900

    [コンフリクト解消]Characterクラスにて、toString()が重複　⇨　Character.name.toStringとしました

--- Main/src/character/Character.java
+++ Main/src/character/Character.java
@@ -17,7 +17,7 @@ public class Character {
     private final Magic magic;
     private final Point point;
 
-    public Character(Name name, HitPoint hitPoint, AttackPoint attackPoint, Type type, Magic magic, Point point){
+    public Character(Name name, HitPoint hitPoint, AttackPoint attackPoint, Type type, Magic magic, Point point) {
         this.name = name;
         this.hitPoint = hitPoint;
         this.attackPoint = attackPoint;
@@ -26,12 +26,15 @@ public class Character {
         this.point = point;
     }
 
-    public Type typeIs(){
+    public Type typeIs() {
         return this.type;
     }
-    public Point pointIs(){ return this.point;}
 
-    private void damaged(final AttackPoint attackPoint){
+    public Point pointIs() {
+        return this.point;
+    }
+
+    private void damaged(final AttackPoint attackPoint) {
         this.hitPoint = this.hitPoint.minus(attackPoint);
     }
 
@@ -39,25 +42,26 @@ public class Character {
     //結果：相手のhpを減少させる
     //式：相手のhp - 自分の攻撃力 - 自分の魔法攻撃力
     //備考：相手の苦手タイプで攻撃した場合、魔法攻撃力が2倍になる
-    public void attack(Character enemy){
+    public void attack(Character enemy) {
         AttackPoint baseMagicAttackPoint = this.magic.getAttackPoint();
 
         boolean compatibilityIsEffective = this.type.getWeakType() == enemy.type.getStrongType();
         boolean compatibilityIsIneffective = this.type.getStrongType() == enemy.type.getWeakType();
 
-        if(!(compatibilityIsEffective && compatibilityIsIneffective)){
+        if (!(compatibilityIsEffective && compatibilityIsIneffective)) {
             enemy.damaged(this.attackPoint.add(baseMagicAttackPoint));
         }
-        if(compatibilityIsEffective){
+        if (compatibilityIsEffective) {
             enemy.damaged(this.attackPoint.add(baseMagicAttackPoint.effective()));
         }
-        if(compatibilityIsIneffective){
+        if (compatibilityIsIneffective) {
             enemy.damaged(this.attackPoint.add(baseMagicAttackPoint.ineffective()));
         }
     }
+
     @Override
-    public boolean equals(Object o){
-        if(!(o instanceof Character object)){
+    public boolean equals(Object o) {
+        if (!(o instanceof Character object)) {
             return false;
         }
         boolean valid = object.name.equals(this.name) && object.hitPoint.equals(this.hitPoint) && object.attackPoint.equals(this.attackPoint)
@@ -65,7 +69,7 @@ public class Character {
         return valid;
     }
 
-    public String toString(){
+    public String toString() {
         return this.name.toString();
     }
 }
