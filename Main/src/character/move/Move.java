d04bb6f620b8142a620ee29122d19d70dd805437
hayashi <hayashi.takuma0924@gmail.com>
2022-09-18 17:34:23 +0900

    Revert "Merge pull request #10 from hayashi0924/feature_QuestionStream"
    This reverts commit 33a0650db620abda775e272e2c5f1bb3678f3ef6, reversing
    changes made to 72c4f53088fec89305e125cbb925c29b95cce33f.

--- Main/src/character/move/Move.java
+++ Main/src/character/move/Move.java
@@ -6,12 +6,18 @@ import java.util.Map;
 public class Move {
     private final static Map<Direction, Action> move = new HashMap<>();
 
-    public static Action move(Direction direction){
+    private Move(){
+        ;
+    }
+
+    {
         move.put(Direction.UP, new Up());
         move.put(Direction.DOWN, new Down());
         move.put(Direction.RIGHT, new Right());
         move.put(Direction.LEFT, new Left());
+    }
 
+    public static Action move(Direction direction){
         return move.get(direction);
     }
 }
--- Main/src/field/Field.java
+++ Main/src/field/Field.java
@@ -2,22 +2,19 @@ package field;
 
 import character.Character;
 import character.move.Direction;
-import character.move.Move;
 import field.obstacle.Obstacle;
-import field.obstacle.Type;
 import point.Point;
 
-import java.util.stream.Collectors;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Field {
-    private String[][] map;
+    private String[][] map = new String[Range.MAX_ROW][Range.MAX_COLUMN];
     private static final String DOT = ".";
 
     private Field(){
-        this.map = new String[Range.MAX_ROW][Range.MAX_COLUMN];
-        for(int i = 0; i < Range.MAX_ROW; i++){
-            for(int j = 0; j < Range.MAX_COLUMN; j++){
+        for(int i =0; Range.rowIsValid(i - 1); i++){
+            for(int j = 0; Range.columnIsValid(j - 1); j++){
                 map[i][j] = DOT;
             }
         }
@@ -38,36 +35,16 @@ public class Field {
         this.map[point.getRow()][point.getColumn()] = DOT;
     }
 
-    public void on(Character character, Point point){
-        this.map[point.getRow()][point.getColumn()] = character.toString();
+    public void charPoint(Character character){
+        this.map[character.pointIs().getRow()][character.pointIs().getColumn()] = character.toString();
     }
 
-    private boolean canMove(Point point){
-        for(Type types : Type.values()){
-            if(this.map[point.getRow()][point.getColumn()].equals(types.toString())){
-                return false;
-            }
-        }
-        return true;
+    public void charMovement(Character character, Direction direction){
+        this.map[character.pointIs().getRow()][character.pointIs().getColumn()] = DOT;
+        this.map[character.move(direction).getRow()][character.move(direction).getColumn()] = character.toString();
     }
 
-    public void move(Character character, Point point){
-        if(!canMove(point)){
-            throw new IllegalArgumentException("移動先には障害物があります。移動先を変更してください");
-        }
-        for(String[] maps : this.map){
-            Stream.of(maps).filter(characterName -> character.toString().equals(characterName))
-                    .map(beforeMove -> DOT).toArray(String[]::new);
-        }
-        this.map[point.getRow()][point.getColumn()] = character.toString();
-    }
-
-    public void scene() {
-        for(int i = 0; i < Range.MAX_ROW; i++){
-            for(int j = 0; j < Range.MAX_COLUMN; j++){
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }
+    public void scene(){
+        Stream.of(map).forEach(System.out::println);
     }
 }
